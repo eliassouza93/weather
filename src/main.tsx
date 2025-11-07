06f536314ea3c4479d3ce1eb2238315111a3fc94
eliassouza93 <elias99-souza@hotmail.com>
2025-11-07 16:09:12 -0300

    fix: remove componente used

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,6 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { HomePage } from "./page";
 
- 
-
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <HomePage />
